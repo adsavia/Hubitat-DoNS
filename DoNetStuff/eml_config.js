d621a47a253670714ca9ed9d98f0f22bb45545db
adsavia <register@adsavia.com>
2019-08-08 10:38:05 +0000

    start ignoring eml_config.js

--- DoNetStuff/eml_config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-        "<email addr1>": {authuser: "<auth user1>", authpwd: "<auth password1>"}
-        ,"<email addr2>": {authuser: "<auth user2>", authpwd: "<auth password2>"}
-        ,"<email addr3>": {authuser: "<auth user3>", authpwd: "<auth password3>"}
-};
-
