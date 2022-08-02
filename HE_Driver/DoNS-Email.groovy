176bab5ae00f4f3b23f7a6a9d9afcd343e2b0730
adsavia <register@adsavia.com>
2022-08-02 14:41:02 -0400

    Update DoNS-Email.groovy

--- HE_Driver/DoNS-Email.groovy
+++ HE_Driver/DoNS-Email.groovy
@@ -3,20 +3,20 @@
 *   File: DoNS-Email.groovy
 *   Platform: Hubitat
 *   Modification History:
-*       		Date        Time	Who		What
-*       		2018-11-27 			erktrek	Used Dan Ogorchock's pushover DTH for Hubitat HE as a template.
-*       		2018-11-28	10:04	erktrek	Added ability to use single message / to in device.
-*       v1.1.0	2018-11-28	12:05	erktrek	Added bypass to try/catch error if message is "OK"
+*   		Date        	Time	Who		What
+*   		2018-11-27 		erktrek		Used Dan Ogorchock's pushover DTH for Hubitat HE as a template.
+*   		2018-11-28	10:04	erktrek		Added ability to use single message / to in device.
+*       v1.1.0	2018-11-28	12:05	erktrek		Added bypass to try/catch error if message is "OK"
 *									tweaked version to fit work a bit better.
-*       v1.2.0	2019-01-31	08:54	erktrek	Changed call to async for better system performance, tweaked subject to display blank instead of null
-*       v1.2.1	2019-03-25	07:57	erktrek	Minor bug fix, removed "null" subject display for preconfigured email devices.
-*       v2.0.0	2019-08-06	22:31	erktrek	Added Google, smtp and SMTPS transports.
-*       v2.1.0	2019-08-07	08:02	erktrek	Moved auth password to eml_config.js on server. 
-*       v2.1.1	2019-08-07	16:00	erktrek	Updated to add state variable authusers, fixed some bugs re:dup submit 
-*       v2.1.2	2019-08-08	07:00	erktrek	Moved both authuser and authpwd to eml_config.js file, lookup based on From user. 
-*       v2.1.3	2019-08-09	08:00	erktrek	changed state variable authusers to StoredFromAddrs for clarity
-*       v3.0.0	2022-08-01	14:25	erktrek	Updated nodemailer to remove vulnerability, removed node_modules dir, those need to be 
-*									installed via npm, added exec capability.
+*       v1.2.0	2019-01-31	08:54	erktrek		Changed call to async for better system performance, tweaked subject to display blank instead of null
+*       v1.2.1	2019-03-25	07:57	erktrek		Minor bug fix, removed "null" subject display for preconfigured email devices.
+*       v2.0.0	2019-08-06	22:31	erktrek		Added Google, smtp and SMTPS transports.
+*       v2.1.0	2019-08-07	08:02	erktrek		Moved auth password to eml_config.js on server. 
+*       v2.1.1	2019-08-07	16:00	erktrek		Updated to add state variable authusers, fixed some bugs re:dup submit 
+*       v2.1.2	2019-08-08	07:00	erktrek		Moved both authuser and authpwd to eml_config.js file, lookup based on From user. 
+*       v2.1.3	2019-08-09	08:00	erktrek		Changed state variable authusers to StoredFromAddrs for clarity
+*       v3.0.0	2022-08-01	14:25	erktrek		Updated nodemailer to remove vulnerability, removed node_modules dir, those need to be 
+*							installed via npm, added exec capability.
 *
 *  Copyright 2022 erktrek
 *
